ac888e775377351ea58af1f0ab531467f6aede65
Matus Kollarcik <matus.kollarcik@gmail.com>
2021-10-22 21:24:46 +0200

    fix import

--- pages/onboarding/first-step.tsx
+++ pages/onboarding/first-step.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 
-import { FirstStep } from '../../src/components/onboarding/first-step'
+import { FirstStep } from '../../src/components/onboarding'
 
 export default function Creators() {
   const { isLoading, isAuthenticated } = useAuth0()
